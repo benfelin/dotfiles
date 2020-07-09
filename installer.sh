12d21d65058ce4c744f09e7ceb99381fe1d2ea1d
benfelin <4267892+benfelin@users.noreply.github.com>
2020-07-09 22:52:20 +0100

    Backup original dotfiles before stow

--- installer.sh
+++ installer.sh
@@ -1,6 +1,12 @@
 #!/bin/sh 
 
+cd ~
+[ -f .bashrc ] && mv .bashrc .bashrc.ini
+[ -f .bash_logout ] && mv .bash_logout .bash_logout.ini
+[ -f .profile ] && mv .profile .profile.ini
+cd -
 stow bash
 stow tmux
+stow vim
 stow git
 stow X
