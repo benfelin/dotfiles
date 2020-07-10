0313b47b9295bc868cd7fbeea4304da2a498079d
benfelin <4267892+benfelin@users.noreply.github.com>
2020-07-10 15:12:58 +0100

    Add Bundle plugins installation

--- installer.sh
+++ installer.sh
@@ -2,13 +2,18 @@
 
 git submodule init
 git submodule update
-cd ~
+
+cd $HOME
 [ -f .bashrc ] && mv .bashrc .bashrc.ini
 [ -f .bash_logout ] && mv .bash_logout .bash_logout.ini
 [ -f .profile ] && mv .profile .profile.ini
 cd -
+
 stow bash
 stow tmux
-stow vim
 stow git
 stow X
+
+stow vim
+vim +BundleInstall +qall
+ln -s $HOME/.vim/bundle/vim-colorschemes/colors $HOME/.vim/colors 
