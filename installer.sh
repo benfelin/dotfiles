2908842def0993dd2a79e292d612382f9228af22
bbb <4267892+benfelin@users.noreply.github.com>
2021-09-23 15:54:33 +0100

    install script: backup regular files that are not symlinks

--- installer.sh
+++ installer.sh
@@ -5,20 +5,22 @@ command -v stow >/dev/null 2>&1 || { echo "Install GNU Stow first"; exit 1; }
 git submodule init
 git submodule update
 
-test -f $HOME/.bashrc && mv $HOME/.bashrc $HOME/.bashrc.ini
-test -f $HOME/.bash_aliases && mv $HOME/.bash_aliases $HOME/.bash_aliases.ini
-test -f $HOME/.bash_functions && mv $HOME/.bash_functions $HOME/.bash_functions.ini
-test -f $HOME/.profile && mv $HOME/.profile $HOME/.profile.ini
-test -f $HOME/.bash_logout && mv $HOME/.bash_logout $HOME/.bash_logout.ini
+test ! -h $HOME/.bashrc && mv $HOME/.bashrc $HOME/.bashrc.ini
+test ! -h $HOME/.bash_aliases && mv $HOME/.bash_aliases $HOME/.bash_aliases.ini
+test ! -h $HOME/.bash_functions && mv $HOME/.bash_functions $HOME/.bash_functions.ini
+test ! -h $HOME/.profile && mv $HOME/.profile $HOME/.profile.ini
+test ! -h $HOME/.bash_logout && mv $HOME/.bash_logout $HOME/.bash_logout.ini
 stow bash
 
 stow git
 
 stow perl
 
-[ -f $HOME/.tmux.conf ] && mv $HOME/.tmux.conf $HOME/.tmux.conf.ini
+[ ! -h $HOME/.tmux.conf ] && mv $HOME/.tmux.conf $HOME/.tmux.conf.ini
 stow tmux
 
 stow vim
 vim +BundleInstall +qall
 test -d $HOME/.vim/colors || ln -s $HOME/.vim/bundle/vim-colorschemes/colors $HOME/.vim/colors
+
+stow X
