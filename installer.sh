e1ffe484c54224bdd0384be591d0e98ab88649ff
bbb <4267892+benfelin@users.noreply.github.com>
2021-09-24 11:10:08 +0100

    installer: cleanup

--- installer.sh
+++ installer.sh
@@ -2,30 +2,22 @@
 
 command -v stow >/dev/null 2>&1 || { echo "Install GNU Stow first"; exit 1; }
 
+backup () {
+	for FILE in "$@"
+    do
+        test -e $HOME/$FILE -a ! -h $HOME/$FILE  && mv $HOME/$FILE $HOME/${FILE}.ini
+    done
+}
+
 git submodule init
 git submodule update
 
-test ! -h $HOME/.bashrc && mv $HOME/.bashrc $HOME/.bashrc.ini
-test ! -h $HOME/.bash_aliases && mv $HOME/.bash_aliases $HOME/.bash_aliases.ini
-test ! -h $HOME/.bash_functions && mv $HOME/.bash_functions $HOME/.bash_functions.ini
-test ! -h $HOME/.profile && mv $HOME/.profile $HOME/.profile.ini
-test ! -h $HOME/.bash_logout && mv $HOME/.bash_logout $HOME/.bash_logout.ini
-stow bash
-
-stow git
-
-stow perl
-
-[ ! -h $HOME/.tmux.conf ] && mv $HOME/.tmux.conf $HOME/.tmux.conf.ini
-stow tmux
+backup .xinitrc .xsession .xprofile .Xdefaults .Xresources;         stow X
+backup .bashrc .bash_aliases .bash_functions .profile .bash_logout; stow bash
+backup .tmux.conf;                                                  stow tmux
+                                                                    stow git
+                                                                    stow perl
 
 stow vim
 vim +BundleInstall +qall
 test -d $HOME/.vim/colors || ln -s $HOME/.vim/bundle/vim-colorschemes/colors $HOME/.vim/colors
-
-test ! -h $HOME/.xinitrc && mv $HOME/.xinitrc $HOME/.xinitrc.ini
-test ! -h $HOME/.xsession && mv $HOME/.xsession $HOME/.xsession.ini
-test ! -h $HOME/.xprofile && mv $HOME/.xprofile $HOME/.xprofile.ini
-test ! -h $HOME/.Xdefaults && mv $HOME/.Xdefaults $HOME/.Xdefaults.ini
-test ! -h $HOME/.Xresources && mv $HOME/.Xresources $HOME/.Xresources.ini
-stow X
