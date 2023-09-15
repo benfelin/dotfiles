c08b0820041833095c54be5bcdf6afe69b545ee0
bbb <4267892+benfelin@users.noreply.github.com>
2023-09-15 16:55:04 +0100

    refactor: how vim/tmux deal with their plugin manager

--- installer.sh
+++ installer.sh
@@ -6,21 +6,27 @@ for i in bash tmux git perl vim; do
         command -v $i >/dev/null || echo "Please install $i"
 done
 
+mkdir -p $HOME/.dotfiles-backup
+
 backup () {
 	for FILE in "$@"
     do
-        test -e $HOME/$FILE -a ! -h $HOME/$FILE  && mv $HOME/$FILE $HOME/${FILE}.ini
+        test ! -h $HOME/$FILE -a -e $HOME/$FILE && mv $HOME/$FILE $HOME/.dotfiles-backup/
     done
 }
 
 git submodule update --init --recursive
 
-backup .xinitrc .xsession .xprofile .Xdefaults .Xresources;         stow --dotfiles X
-backup .bashrc .bash_aliases .bash_functions .profile .bash_logout; stow --dotfiles bash
-backup .tmux.conf;                                                  stow --dotfiles tmux
-backup .screenrc;                                                   stow --dotfiles screen
-                                                                    stow git
-                                                                    stow --dotfiles perl
-backup .vimrc;                                                      stow --dotfiles vim
+backup .xinitrc .xsession .xprofile .Xdefaults .Xresources;         stow --dotfiles 			    X
+backup .bashrc .bash_aliases .bash_functions .profile .bash_logout; stow --dotfiles 			    bash
+backup .tmux.conf;                                                  stow --dotfiles --no-folding 	tmux
+backup .screenrc;                                                   stow --dotfiles 			    screen
+                                                                    stow --dotfiles	--no-folding 	git
+                                                                    stow --dotfiles 			    perl
+backup .vimrc;                                                      stow --dotfiles 			    vim
+mkdir -p $HOME/.vim/bundle
+test ! -e $HOME/.vim/bundle/Vundle.vim && ln -s $HOME/.dotfiles/plugins/Vundle.vim $HOME/.vim/bundle/
+test ! -e $HOME/.vim/colors && ln -s $HOME/.dotfiles/plugins/vim-colorschemes/colors $HOME/.vim/
 vim +BundleInstall +qall
-test -d $HOME/.vim/colors || ln -s $HOME/.vim/bundle/vim-colorschemes/colors $HOME/.vim/colors
+mkdir -p $HOME/.tmux/plugins
+test ! -e $HOME/.tmux/plugins/tpm && ln -s $HOME/.dotfiles/plugins/tpm $HOME/.tmux/plugins/
--- tmux/.local/bin/tmux-resetkeys.sh
+++ tmux/.local/bin/tmux-resetkeys.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 tmux -f /dev/null -L temp start-server \; list-keys | \
     sed -r \
     -e "s/(bind-key.*\s+)([\"#~\$])(\s+)/\1\'\2\'\3/g" \
@@ -6,4 +7,6 @@ tmux -f /dev/null -L temp start-server \; list-keys | \
     -e "s/(bind-key.*\s+)([;])(\s+)/\1\\\\\2\3/g" \
     -e "s/(command-prompt -I )#([SW])/\1\"#\2\"/g" \
     -e "s/(if-shell -F -t = )#([^ ]+)/\1\"#\2\"/g" \
-    > $HOME/.tmux.reset.conf                              
+    > $HOME/.tmux-defaultkeys_$(tmux -V | sed 's/^tmux //').conf                              
+
+ln -sf $HOME/.tmux-defaultkeys_*.conf $HOME/.tmux-defaultkeys.conf
