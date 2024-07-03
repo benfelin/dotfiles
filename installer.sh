cbfca05c97496dab3bcb5334ffc4790dda783477
Benoit Felix <bf5@sanger.ac.uk>
2024-07-03 16:33:14 +0100

    refactor(installer): better use of GNU stow
    Now using those two options:
    
    --dotfiles
               Enable special handling for "dotfiles" (files or folders whose name begins with a period) in the package directory. If this option is enabled,
               Stow will add a preprocessing step for each file or folder whose name begins with "dot-", and replace the "dot-" prefix in the name by a period
               (.). This is useful when Stow is used to manage collections of dotfiles, to avoid having a package directory full of hidden files.
    
               For example, suppose we have a package containing two files, stow/dot-bashrc and stow/dot-emacs.d/init.el. With this option, Stow will create
               symlinks from .bashrc to stow/dot-bashrc and from .emacs.d/init.el to stow/dot-emacs.d/init.el. Any other files, whose name does not begin with
               "dot-", will be processed as usual.
    
    --no-folding
               Disable folding of newly stowed directories when stowing, and refolding of newly foldable directories when unstowing.
    
    The default action of Stow is to install a package. This means creating symlinks in the target tree that point into the package tree.  Stow attempts to do this with as few symlinks as possible; in other words, if Stow can create a single symlink that points to an entire subtree within the package tree, it will choose to do that rather than create a directory in the target tree and populate it with symlinks.
    The problem with tree-folding is that extraneous files can be added on behalf of the users (e.g. hooks in git folder) and, then, make management of the repository difficult (infinite gitignore)

--- installer.sh
+++ installer.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
 
-command -v stow >/dev/null 2>&1 || { echo "Install GNU Stow first"; exit 1; }
+command -v stow >/dev/null 2>&1 || {
+	echo "Install GNU Stow first"
+	exit 1
+}
 
 for i in bash tmux git perl vim; do
-        command -v $i >/dev/null || echo "Please install $i"
+	command -v $i >/dev/null || echo "Please install $i"
 done
 
-backup () {
-	for FILE in "$@"
-    do
-        test -e $HOME/$FILE -a ! -h $HOME/$FILE  && mv $HOME/$FILE $HOME/${FILE}.ini
-    done
+backup() {
+	for FILE in "$@"; do
+		test -e "$HOME"/"$FILE" -a ! -h "$HOME"/"$FILE" && mv "$HOME"/"$FILE" "$HOME"/"$FILE".ini
+	done
 }
 
 git submodule init
 git submodule update
 
-backup .Xdefaults .Xresources;                                      stow --dotfiles              X
-backup .bashrc .bash_aliases .bash_functions .profile .bash_logout; stow --dotfiles              bash
-backup .tmux.conf;                                                  stow --dotfiles --no-folding tmux
-                                                                    stow --dotfiles --no-folding git
-                                                                    stow --dotfiles              perl
-backup .vimrc;                                                      stow --dotfiles --no-folding vim
+backup .Xdefaults .Xresources
+stow --dotfiles X
+backup .bashrc .bash_aliases .bash_functions .profile .bash_logout
+stow --dotfiles bash
+backup .tmux.conf
+stow --dotfiles --no-folding tmux
+stow --dotfiles --no-folding git
+stow --dotfiles perl
+backup .vimrc
+stow --dotfiles --no-folding vim
 vim +BundleInstall +qall
-test -d $HOME/.vim/colors || ln -s $HOME/.vim/bundle/vim-colorschemes/colors $HOME/.vim/colors
+test -d "$HOME"/.vim/colors || ln -s "$HOME"/.vim/bundle/vim-colorschemes/colors "$HOME"/.vim/colors
